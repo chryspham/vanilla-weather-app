abc420ae6393cc44b48b35f11406fdc61ea32d2f
chryspham <chrysabel.pham@gmail.com>
2023-01-12 20:47:53 -0800

    updated humidity, wind speed, and time

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
+function getTime(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hour}:${minutes}`;
+}
+
 function getTemperature(response) {
   console.log(response.data);
   let tempElement = document.querySelector("#numTemp");
   tempElement.innerHTML = Math.round(response.data.main.temp);
+
   let cityElement = document.querySelectorAll("#cityName");
   cityElement.innerHTML = response.data.main.name;
+
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].main;
+
+  let displayTime = document.querySelector("#displayTime");
+  displayTime.innerHTML = getTime(response.data.dt * 1000);
+
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = response.data.wind.speed;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey = "5ef4de8cd6b7fefcd7c42f98cf464ce8";
